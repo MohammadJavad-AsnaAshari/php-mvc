d1969951744a1f59790085b67b10a55a9b764809
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-02-01 17:52:50 +0330

    refactor: Improve modularity and readability in Router class.

--- pocket/src/Router.php
+++ pocket/src/Router.php
@@ -23,33 +23,30 @@ class Router
 
         if ($method === 'get') {
             $params = [];
-            if (isset($this->routeMap[$method][$url])) {
-                $callback = $this->routeMap[$method][$url];
-            } else {
-                $routeCallback = $this->getCallbackFromDynamicRoute();
-                if (!$routeCallback) {
-                    throw new \Exception('Not Found 404');
-                }
-
-                $callback = $routeCallback[0];
-                $params = $routeCallback[1];
-            }
-            return call_user_func($callback, ...array_values($params));
+            return $this->getCallbackForUrl($method, $url, $params);
         }
 
         return 'The `METHOD` is not set yet! 404';
     }
 
-    private function getCallbackFromDynamicRoute()
+    private function getCallbackForUrl(string $method, string $url, mixed $params): mixed
     {
-        $method = strtolower($_SERVER["REQUEST_METHOD"]);
-        $url = strtolower($_SERVER['REQUEST_URI']);
-        $pos = strpos($url, '?'); // ? position
+        if (isset($this->routeMap[$method][$url])) {
+            $callback = $this->routeMap[$method][$url];
+        } else {
+            $routeCallback = $this->getCallbackFromDynamicRoute($method, $url);
+            if (!$routeCallback) {
+                throw new \Exception('Not Found 404');
+            }
 
-        if ($pos) {
-            $url = substr($url, 0, $pos);
+            $callback = $routeCallback[0];
+            $params = $routeCallback[1];
         }
+        return call_user_func($callback, ...array_values($params));
+    }
 
+    private function getCallbackFromDynamicRoute(string $method, string $url)
+    {
         $routes = $this->routeMap[$method];
 
         foreach ($routes as $route => $callback) {
@@ -75,7 +72,7 @@ class Router
 
                 $routeParams = array_combine($routeNames, $values);
 
-                return [0 => $callback, 1 => $routeParams];
+                return [$callback, $routeParams];
             }
         }
 
