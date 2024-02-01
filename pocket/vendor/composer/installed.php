c7fb023ec3ee69f229c283c102a5b033811148a7
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-02-01 11:53:42 +0330

    feat: Implement `Pocket` package and require it in main `composer.json`.

---
+++ pocket/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mj/pocket-core',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd70657b7bfbbc1ad3c6f995d9f85563b77e9bb4e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mj/pocket-core' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd70657b7bfbbc1ad3c6f995d9f85563b77e9bb4e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
