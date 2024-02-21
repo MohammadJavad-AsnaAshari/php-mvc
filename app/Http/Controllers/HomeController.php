e991d56ef59048e9c730842c9069ac394ff6e84f
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-02-21 09:18:48 +0330

    feat: Add home page route.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController
+{
+    public function index(): string
+    {
+        return 'this is home page';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Mj\PocketCore\Router;
 
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+Router::get('/', [HomeController::class, 'index']);
 
 Router::get('/article/{id:\d+}', [ArticleController::class, 'dynamic']);
 
