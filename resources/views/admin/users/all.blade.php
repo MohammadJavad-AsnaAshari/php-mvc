efc8558764e818d0853e7805161bf1304fd10e64
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-03-07 12:20:47 +0330

    feat: Clear front-end layouts and structure and add all users panel.

---
+++ resources/views/admin/users/all.blade.php
@@ -0,0 +1,8 @@
+@component('admin.layouts.content', ['title' => 'All Users'])
+
+    @slot('breadcrumb')
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">All Users</li>
+    @endslot
+
+@endcomponent
--- routes/admin.php
+++ routes/admin.php
@@ -3,3 +3,4 @@
 use Mj\PocketCore\Router;
 
 Router::get('/admin', 'admin.index');
+Router::get('/admin/users', 'admin.users.all');
