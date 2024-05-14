6aecaf26a3d11dc5eb964bae741a1ca2508c1d0a
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-05-15 02:29:41 +0330

    feat: Add login and register button to forms and download necessary css file too.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Login')
 
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link href="/css/tailwind.min.css" rel="stylesheet">
 
 {{--@section('body')--}}
 <body class="bg-gray-700">
@@ -66,6 +66,10 @@
                     </button>
                 </div>
             </form>
+            <a href="/auth/register"
+               class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Register
+            </a>
         </div>
     </div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Register')
 
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link href="/css/tailwind.min.css" rel="stylesheet">
 
 {{--@section('body')--}}
 <body class=" bg-gray-700">
@@ -79,6 +79,10 @@
                     </button>
                 </div>
             </form>
+            <a href="/auth/login"
+               class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Login
+            </a>
         </div>
     </div>
 </div>
