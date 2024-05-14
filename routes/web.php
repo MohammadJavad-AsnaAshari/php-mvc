4f5f7a9c0b9ecb2f3380a7ac8a73e4e201f05db3
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-05-15 02:08:51 +0330

    refactor: Remove unnecessary codes.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Client\HomeController;
-use App\Http\Controllers\UserController;
 use Mj\PocketCore\Router;
 
 Router::get('/', [HomeController::class, 'indexHome']);
@@ -21,13 +19,6 @@ Router::get('/panel', function () {
     if (auth()->check()) {
         dd(auth()->user()->name);
     }
-}, [\App\Http\Middlewares\AuthMiddleware::class]);
-
-Router::get('/article/{id:\d+}', [ArticleController::class, 'dynamic']);
-Router::get('/article', [ArticleController::class, 'index']);
-Router::get('/article/create', [ArticleController::class, 'create']);
-Router::post('/article/create', [ArticleController::class, 'store']);
-
-Router::get('/user/create', [UserController::class, 'create']);
-Router::get('/user/update/{id}', [UserController::class, 'update']);
-Router::get('/user/delete/{id}', [UserController::class, 'delete']);
+},
+    [\App\Http\Middlewares\AuthMiddleware::class]
+);
