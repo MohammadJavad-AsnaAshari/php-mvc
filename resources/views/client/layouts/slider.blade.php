9d0f7855b993f49469a136bbb40b7b0f45b5ef0b
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-06-10 22:41:57 +0330

    feat: Implement Slider view.

--- resources/views/client/layouts/slider.blade.php
+++ resources/views/client/layouts/slider.blade.php
@@ -2,103 +2,31 @@
     <div class="slider_container">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div class="detail-box">
-                                    <h1>
-                                        Welcome To Our <br>
-                                        <?=$_ENV['APP_NAME']?> 1
-                                    </h1>
-                                    <p>
-                                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
-                                        necessitatibus error
-                                        distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio
-                                        esse, possimus
-                                        maiores aliquid repellat beatae cum, perspiciatis enim, accusantium
-                                        perferendis.
-                                    </p>
-                                    <a href="">
-                                        Contact Us
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-md-5 ">
-                                <div class="img-box">
-                                    <img src="images/slider-img.png" alt=""/>
-                                </div>
-                            </div>
+                @php
+                    $directory = ROOT . 'public/images/slider';
+                    $images = scandir($directory);
+                    $i = 0;
+                @endphp
+                @foreach ($images as $image)
+                    @if ($image !== '.' && $image !== '..')
+                        @php
+                            $active = $i === 0 ? 'active' : '';
+                        @endphp
+                        <div class="carousel-item {{ $active }}">
+                            <img src="{{ 'images/slider/' . $image }}" class="d-block w-100 img-fluid" alt="Description of {{ $image }}">
                         </div>
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div class="detail-box">
-                                    <h1>
-                                        Welcome To Our <br>
-                                        <?=$_ENV['APP_NAME']?> 2
-                                    </h1>
-                                    <p>
-                                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
-                                        necessitatibus error
-                                        distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio
-                                        esse, possimus
-                                        maiores aliquid repellat beatae cum, perspiciatis enim, accusantium
-                                        perferendis.
-                                    </p>
-                                    <a href="">
-                                        Contact Us
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-md-5 ">
-                                <div class="img-box">
-                                    <img src="images/slider-img.png" alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div class="detail-box">
-                                    <h1>
-                                        Welcome To Our <br>
-                                        <?=$_ENV['APP_NAME']?> 3
-                                    </h1>
-                                    <p>
-                                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
-                                        necessitatibus error
-                                        distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio
-                                        esse, possimus
-                                        maiores aliquid repellat beatae cum, perspiciatis enim, accusantium
-                                        perferendis.
-                                    </p>
-                                    <a href="">
-                                        Contact Us
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-md-5 ">
-                                <div class="img-box">
-                                    <img src="images/slider-img.png" alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        @php
+                            $i++;
+                        @endphp
+                    @endif
+                @endforeach
             </div>
             <div class="carousel_btn-box">
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                     <i class="fa fa-arrow-left" aria-hidden="true"></i>
                     <span class="sr-only">Previous</span>
                 </a>
-                <img src="images/line.png" alt=""/>
+                <img src="{{ 'images/line.png' }}" alt=""/>
                 <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                     <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     <span class="sr-only">Next</span>
