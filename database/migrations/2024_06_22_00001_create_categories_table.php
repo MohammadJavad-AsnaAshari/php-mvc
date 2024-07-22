4c1e095b33ce1ffda3fc9c6a3f29448a9e06c69f
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-07-23 00:02:18 +0330

    fix: remove unnecessary parent_id from categories table migration.

--- database/migrations/2024_06_22_00001_create_categories_table.php
+++ database/migrations/2024_06_22_00001_create_categories_table.php
@@ -13,11 +13,9 @@ return new class{
     {
         $sql = "CREATE TABLE IF NOT EXISTS `categories` (
               `id` INT AUTO_INCREMENT PRIMARY KEY,
-              `parent_id` INT DEFAULT NULL,
               `name` VARCHAR(255) NOT NULL UNIQUE,
               `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
               `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-              CONSTRAINT `fk_categories_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `categories`(`id`)
         );";
 
         $this->database->pdo->exec($sql);
