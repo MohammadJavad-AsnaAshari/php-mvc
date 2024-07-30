06532371ceb98554fe74942af369917abdbad3ba
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-07-30 18:28:20 +0330

    fix: Remove `,` from categories migration sql.

--- database/migrations/2024_06_22_00001_create_categories_table.php
+++ database/migrations/2024_06_22_00001_create_categories_table.php
@@ -15,7 +15,7 @@ return new class{
               `id` INT AUTO_INCREMENT PRIMARY KEY,
               `name` VARCHAR(255) NOT NULL UNIQUE,
               `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-              `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );";
 
         $this->database->pdo->exec($sql);
